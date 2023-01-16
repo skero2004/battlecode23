96fe6bff7bc1240971f201cb7af82280abd4b80e
Mohit Hulse <hulsemohit@gmail.com>
2023-01-15 22:05:56 -0500

    created bot0

---
+++ src/bot0/Headquarter.java
@@ -0,0 +1,31 @@
+package examplefuncsplayer;
+
+import battlecode.common.*;
+
+public class Headquarter {
+
+    static void runHeadquarters(RobotController rc) throws GameActionException {
+        // Pick a direction to build in.
+        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        MapLocation newLoc = rc.getLocation().add(dir);
+        if (rc.canBuildAnchor(Anchor.STANDARD)) {
+            // If we can build an anchor do it!
+            rc.buildAnchor(Anchor.STANDARD);
+            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
+        }
+        if (RobotPlayer.rng.nextBoolean()) {
+            // Let's try to build a carrier.
+            rc.setIndicatorString("Trying to build a carrier");
+            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+                rc.buildRobot(RobotType.CARRIER, newLoc);
+            }
+        } else {
+            // Let's try to build a launcher.
+            rc.setIndicatorString("Trying to build a launcher");
+            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            }
+        }
+    }
+
+}
