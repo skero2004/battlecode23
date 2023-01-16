4e588d5c239d43d63d3a4d78a5b5e3017ca95b94
Mohit Hulse <hulsemohit@gmail.com>
2023-01-16 16:34:41 -0500

    removed communication

--- src/bot2/Pathing.java
+++ src/bot2/Pathing.java
@@ -22,28 +22,26 @@ public class Pathing {
     switch (type) {
       case WELL_AD:
         for (WellInfo w : rc.senseNearbyWells(ResourceType.ADAMANTIUM)) {
-          if (Communication.setItem(rc, new Well(LocationType.WELL_AD, new Vec2D(w.getMapLocation()), w.isUpgraded())))
+ //         if (Communication.setItem(rc, new Well(LocationType.WELL_AD, new Vec2D(w.getMapLocation()), w.isUpgraded())))
             locations.add(new Vec2D(w.getMapLocation()));
         }
         break;
       case WELL_MN:
         for (WellInfo w : rc.senseNearbyWells(ResourceType.MANA)) {
-          if (Communication.setItem(rc, new Well(LocationType.WELL_AD, new Vec2D(w.getMapLocation()), w.isUpgraded())))
+   //       if (Communication.setItem(rc, new Well(LocationType.WELL_AD, new Vec2D(w.getMapLocation()), w.isUpgraded())))
             locations.add(new Vec2D(w.getMapLocation()));
         }
         break;
       case WELL_EX:
         for (WellInfo w : rc.senseNearbyWells(ResourceType.ELIXIR)) {
-          if (Communication.setItem(rc, new Well(LocationType.WELL_AD, new Vec2D(w.getMapLocation()), w.isUpgraded())))
+     //     if (Communication.setItem(rc, new Well(LocationType.WELL_AD, new Vec2D(w.getMapLocation()), w.isUpgraded())))
             locations.add(new Vec2D(w.getMapLocation()));
         }
         break;
       case ISLAND:
         for (int i : rc.senseNearbyIslands()) {
           for (MapLocation l : rc.senseNearbyIslandLocations(i)) {
-            if (Communication.setItem(rc,
-                new Island(LocationType.ISLAND, new Vec2D(l), rc.senseTeamOccupyingIsland(i) == rc.getTeam(), false,
-                    false)))
+//            if (Communication.setItem(rc, new Island(LocationType.ISLAND, new Vec2D(l), rc.senseTeamOccupyingIsland(i) == rc.getTeam(), false, false)))
               locations.add(new Vec2D(l));
             break;
           }
@@ -52,7 +50,7 @@ public class Pathing {
       case HEADQUARTERS:
         for (RobotInfo r : rc.senseNearbyRobots()) {
           if (r.getTeam() == rc.getTeam() && r.getType() == RobotType.HEADQUARTERS) {
-          if (Communication.setItem(rc, new Headquarters(LocationType.HEADQUARTERS, new Vec2D(r.getLocation()))))
+//          if (Communication.setItem(rc, new Headquarters(LocationType.HEADQUARTERS, new Vec2D(r.getLocation()))))
             locations.add(new Vec2D(r.getLocation()));
           }
         }
@@ -64,16 +62,17 @@ public class Pathing {
 
     for (MapInfo m : rc.senseNearbyMapInfos()) {
       if (m.isPassable())
-        locations.add(current.scale(2).sub(new Vec2D(m.getMapLocation())));
+        ; // locations.add(current.scale(2).sub(new Vec2D(m.getMapLocation())));
     }
 
     for (Vec2D v : locations) {
       Vec2D diff = v.sub(current);
+      System.out.println("Calculaing gravity: " + v.x + ", " + v.y);
       int l = diff.length();
       dir = dir.add(diff.scale(MASS / (1 + l * l * l)));
     }
 
-    System.out.println("Tried dir: " + dir.x + ", "  + dir.y);
+    // System.out.println("Tried dir: " + dir.x + ", "  + dir.y);
     MapLocation to = rc.getLocation().translate(dir.x, dir.y);
     Direction move = rc.getLocation().directionTo(to);
     if (rc.canMove(move) && move != Direction.CENTER)
