d34d4baf24f1a4335f772e72f62fa61b9ff42ead
Mohit Hulse <hulsemohit@gmail.com>
2023-01-16 00:25:47 -0500

    created bot1

---
+++ src/bot1/Headquarter.java
@@ -0,0 +1,39 @@
+package bot1;
+
+import battlecode.common.*;
+
+public class Headquarter {
+
+static void runHeadquarters(RobotController rc) throws GameActionException {
+
+        // Pick a direction to build in.
+        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        MapLocation newLoc = rc.getLocation().add(dir);
+        if (RobotPlayer.turnCount == 1) {
+            Communication.addHeadquarter(rc);
+        } else if (RobotPlayer.turnCount == 2) {
+            Communication.updateHeadquarterInfo(rc);
+        }
+        if (rc.canBuildAnchor(Anchor.STANDARD) && rc.getResourceAmount(ResourceType.ADAMANTIUM) > 100) {
+            // If we can build an anchor do it!
+            rc.buildAnchor(Anchor.STANDARD);
+            rc.setIndicatorString("Building anchor! " + rc.getNumAnchors(Anchor.STANDARD));
+        }
+        if (RobotPlayer.rng.nextBoolean()) {
+            // Let's try to build a carrier.
+            rc.setIndicatorString("Trying to build a carrier");
+            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+                rc.buildRobot(RobotType.CARRIER, newLoc);
+            }
+        } else {
+            // Let's try to build a launcher.
+            rc.setIndicatorString("Trying to build a launcher");
+            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            }
+        }
+        Communication.tryWriteMessages(rc);
+
+    }
+
+}
---
+++ src/bot1/Launcher.java
@@ -0,0 +1,80 @@
+package bot1;
+
+import battlecode.common.*;
+
+public class Launcher {
+
+    static void runLauncher(RobotController rc) throws GameActionException {
+
+        // Try to attack someone
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        int bestScore = -1;
+        RobotInfo target = null;
+        if (RobotPlayer.turnCount == 2) {
+            Communication.updateHeadquarterInfo(rc);
+        }
+        Communication.clearObsoleteEnemies(rc);
+        if (enemies.length > 0) {
+            for (RobotInfo enemy: enemies) {
+                int score = (50 + RobotPlayer.rng.nextInt(50)) * enemy.getHealth();
+                switch (enemy.getType()) {
+                case BOOSTER:
+                case DESTABILIZER:
+                    score *= 1;
+                    break;
+                case LAUNCHER:
+                    score *= 1;
+                    break;
+                case CARRIER:
+                    score *= 1;
+                    break;
+                case AMPLIFIER:
+                    score *= 1;
+                    break;
+                default:
+                    score *= 1;
+                    break;
+                }
+                if (score > bestScore) {
+                    target = enemy;
+                }
+            }
+        }
+        Communication.tryWriteMessages(rc);
+        if (target != null){
+            if (rc.canAttack(target.getLocation()))
+            rc.attack(target.getLocation());
+        }
+        else {
+            WellInfo[] wells = rc.senseNearbyWells();
+            if (wells.length > 0){
+                MapLocation wellLoc = wells[0].getMapLocation();
+                Direction dir = rc.getLocation().directionTo(wellLoc);
+                if (rc.canMove(dir))
+                rc.move(dir);
+            }
+        }
+
+        RobotInfo[] visibleEnemies = rc.senseNearbyRobots(-1, opponent);
+        for (RobotInfo enemy : visibleEnemies) {
+            if (enemy.getType() != RobotType.HEADQUARTERS) {
+                MapLocation enemyLocation = enemy.getLocation();
+                MapLocation robotLocation = rc.getLocation();
+                Direction moveDir = robotLocation.directionTo(enemyLocation);
+                if (rc.canMove(moveDir)) {
+                    rc.move(moveDir);
+                }
+            }
+        }
+
+        // Also try to move randomly.
+        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+
+    }
+
+}
