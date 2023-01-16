b2e6a5f378389b7f2717b070f08ade7ff9090038
skero2004 <toyatakahashi522@gmail.com>
2023-01-16 16:38:40 -0500

    Kill amplifiers!!

--- src/bot2/Headquarter.java
+++ src/bot2/Headquarter.java
@@ -6,16 +6,16 @@ public class Headquarter {
 
     static void runHeadquarter(RobotController rc) throws GameActionException {
 
-        final double[] ROBOT_PROBS = { 0.4, 0.8 }; // Carrier, Launcher
+        final double[] ROBOT_PROBS = { 0.4, 0.9 }; // Carrier, Launcher
         final int MAX_ANCHORS = 10;
         final int MIN_RESOURCES = 400;
 
         // Pick a direction to build in.
         Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
         MapLocation newLoc = rc.getLocation().add(dir);
-        if (rc.canBuildAnchor(Anchor.STANDARD) &&
-            rc.getResourceAmount(ResourceType.ADAMANTIUM) + rc.getResourceAmount(ResourceType.MANA) + rc.getResourceAmount(ResourceType.ELIXIR) > MIN_RESOURCES &&
-            rc.getNumAnchors(Anchor.STANDARD) + rc.getNumAnchors(Anchor.ACCELERATING) < MAX_ANCHORS) {
+        if (rc.canBuildAnchor(Anchor.STANDARD)) {// &&
+            //rc.getResourceAmount(ResourceType.ADAMANTIUM) + rc.getResourceAmount(ResourceType.MANA) + rc.getResourceAmount(ResourceType.ELIXIR) > MIN_RESOURCES &&
+            //rc.getNumAnchors(Anchor.STANDARD) + rc.getNumAnchors(Anchor.ACCELERATING) < MAX_ANCHORS) {
 
             // Build an anchor if we can, if we have enough resources, and if we have less anchors than we want
             rc.buildAnchor(Anchor.STANDARD);
@@ -32,7 +32,7 @@ public class Headquarter {
                 rc.buildRobot(RobotType.CARRIER, newLoc);
             }
 
-        } else if (rand < ROBOT_PROBS[1]) {
+        } else {//if (rand < ROBOT_PROBS[1]) {
 
             // Let's try to build a launcher.
             rc.setIndicatorString("Trying to build a launcher");
@@ -40,7 +40,7 @@ public class Headquarter {
                 rc.buildRobot(RobotType.LAUNCHER, newLoc);
             }
 
-        } else {
+        } /*else {
 
              // Let's try to build an amplifier
             rc.setIndicatorString("Trying to build an amplifier");
@@ -49,7 +49,7 @@ public class Headquarter {
             }
 
 
-        }
+        }*/
 
     }
 
