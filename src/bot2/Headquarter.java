50d08ee81452d615ed18061a38f0103045cfecbb
skero2004 <toyatakahashi522@gmail.com>
2023-01-16 17:38:16 -0500

    Well target things

--- src/bot2/Headquarter.java
+++ src/bot2/Headquarter.java
@@ -5,7 +5,28 @@ import battlecode.common.*;
 public class Headquarter {
 
     static void runHeadquarter(RobotController rc) throws GameActionException {
-
+        // Pick a direction to build in.
+        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        MapLocation newLoc = rc.getLocation().add(dir);
+        if (rc.canBuildAnchor(Anchor.STANDARD) && rc.getResourceAmount(ResourceType.ADAMANTIUM) > 100) {
+            // If we can build an anchor do it!
+            rc.buildAnchor(Anchor.STANDARD);
+            rc.setIndicatorString("Building anchor! " + rc.getNumAnchors(Anchor.STANDARD));
+        }
+        if (RobotPlayer.rng.nextBoolean()) {
+            // Let's try to build a carrier.
+            rc.setIndicatorString("Trying to build a carrier");
+            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+                rc.buildRobot(RobotType.CARRIER, newLoc);
+            }
+        } else {
+            // Let's try to build a launcher.
+            rc.setIndicatorString("Trying to build a launcher");
+            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            }
+        }
+/*
         final double[] ROBOT_PROBS = { 0.5, 0.9 }; // Carrier, Launcher
         final int MAX_ANCHORS = 10;
         final int MIN_RESOURCES = 100;
@@ -50,6 +71,7 @@ public class Headquarter {
 
 
         }*/
+
 
     }
 
