08bd8e42a859a5ad9ba78b99428b10dc20040602
Toya Takahashi <toyatakahashi522@gmail.com>
2023-01-28 15:30:41 -0500

    Wow this beats bot2????

--- src/bot4/Headquarters.java
+++ src/bot4/Headquarters.java
@@ -7,68 +7,66 @@ import bot4.util.*;
 import java.util.Random;
 
 public class Headquarters {
+    static final Random rng = new Random();
+    static final Direction[] directions = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST,
+    };
 
-	static final Random rng = new Random();
-	static final Direction[] directions = {
-			Direction.NORTH,
-			Direction.NORTHEAST,
-			Direction.EAST,
-			Direction.SOUTHEAST,
-			Direction.SOUTH,
-			Direction.SOUTHWEST,
-			Direction.WEST,
-			Direction.NORTHWEST,
-	};
-
-	static final Plan plan = new Plan();
+    static final Plan plan = new Plan();
 
 	/**
 	 * Responsibilities:
 	 * - Choose a new mission from Planning
-	 * - Execute the mission
-	 * - Pass it to Communication
-	 * - Retrieve map info from Communication
-	 * - Pass it to Mapping
-	 */
+     * - Execute the mission 
+     * - Pass it to Communication 
+	 * - Retrieve map info from Communication 
+     * - Pass it to Mapping */
 	static void run(RobotController rc) throws GameActionException {
-		// Choose mission
+        // Choose mission
 		Plan.Mission m1 = plan.chooseMission(rc);
+        
+        // Execute mission
+        RobotType[] rt = {
+            RobotType.LAUNCHER,
+            RobotType.CARRIER,
+            //RobotType.DESTABILIZER,
+            //RobotType.BOOSTER,
+            //RobotType.AMPLIFIER,
+        };
+        int[] rn = {
+            m1.numLauncher,
+            m1.numCarrier,
+            //m1.numDestabilizer,
+            //m1.numBooster,
+            //m1.numAmplifier,
+        };
+        //assert rt.length == rn.length;
 
-		// Execute mission
-		RobotType[] rt = {
-				// RobotType.LAUNCHER,
-				RobotType.CARRIER,
-				// RobotType.DESTABILIZER,
-				// RobotType.BOOSTER,
-				// RobotType.AMPLIFIER,
-		};
-		int[] rn = {
-				// m1.numLauncher,
-				m1.numCarrier,
-				// m1.numDestabilizer,
-				// m1.numBooster,
-				// m1.numAmplifier,
-		};
-		// assert rt.length == rn.length;
+        MapLocation cur = rc.getLocation();
+        for (int i = 0; i < rt.length; ++i) {
+            for (int j = 0; j < rn[i]; ++j) {
+                // Pick a direction to build in.
+                Direction dir = directions[rng.nextInt(directions.length)];
+                MapLocation newLoc = cur.add(dir);
+                if (rc.canBuildRobot(rt[i], newLoc)) {
+                    rc.buildRobot(rt[i], newLoc);
+                }
+            }
+        }
 
-		MapLocation cur = rc.getLocation();
-		for (int i = 0; i < rt.length; ++i) {
-			for (int j = 0; j < rn[i]; ++j) {
-				// Pick a direction to build in.
-				Direction dir = directions[rng.nextInt(directions.length)];
-				MapLocation newLoc = cur.add(dir);
-				if (rc.canBuildRobot(rt[i], newLoc)) {
-					rc.buildRobot(rt[i], newLoc);
-				}
-			}
-		}
-
-		// Communicate mission
-		// Communication.writeMission(m1);
+        // Communicate mission
+		//Communication.writeMission(m1);
 
 		//// TODO how are we planning to distinguish new missions?
-		// Mission m2 = Communication.readMission();
-		// Mapping.addMission(m2);
+		//Mission m2 = Communication.readMission(); 
+		//Mapping.addMission(m2);
 	}
 
 }
--- src/bot4/Launcher.java
+++ src/bot4/Launcher.java
@@ -6,7 +6,90 @@ import bot4.util.*;
 
 public class Launcher {
 
+	static int id = 0;
+	static Direction dir = Constants.directions[0];
+/*
+	static void init(RobotController rc) throws GameActionException {
+
+		id = rc.getID();
+		dir = Constants.directions[id % 8];
+
+	}
+*/
+
 	static void run(RobotController rc) throws GameActionException {
+
+        final int ACTION_RADIUS = rc.getType().actionRadiusSquared;
+        final Team OPPONENT = rc.getTeam().opponent();
+
+		/*
+		// Initialize
+		if (RobotPlayer.turnCount == 1) init(rc);
+
+		// Direction
+		dir = Constants.directions[id % 8];
+		if (rc.canMove(dir))
+			rc.move(dir);
+		else if (rc.getMovementCooldownTurns() - GameConstants.COOLDOWNS_PER_TURN < 0) 
+			if (rc.getID() % 2 == 0) id++;
+			else 					 id--;
+
+        rc.setIndicatorString("" + rc.getMovementCooldownTurns());
+		*/
+
+		Scout.move(rc);
+
+        // Find target enemy
+        RobotInfo[] enemies = rc.senseNearbyRobots(ACTION_RADIUS, OPPONENT);
+        int bestScore = Integer.MIN_VALUE;
+        RobotInfo target = null;
+        for (RobotInfo enemy : enemies) {
+            if (enemy.getType() != RobotType.HEADQUARTERS) {
+                int score = 500 - enemy.getHealth();
+                switch (enemy.getType()) {
+                    case BOOSTER:
+                    case DESTABILIZER:
+                        score *= 3;
+                        break;
+                    case LAUNCHER:
+                        score *= 3;
+                        break;
+                    case CARRIER:
+                        score *= 2;
+                        break;
+                    case AMPLIFIER:
+                        score *= 1;
+                        break;
+                    default:
+                        score *= 1;
+                        break;
+                }
+                if (score > bestScore) {
+                    bestScore = score;
+                    target = enemy;
+                }
+            }
+        }
+
+        RobotInfo[] visibleEnemies = rc.senseNearbyRobots(-1, OPPONENT);
+        for (RobotInfo enemy : visibleEnemies) {
+            if (enemy.getType() != RobotType.HEADQUARTERS) {
+                MapLocation enemyLocation = enemy.getLocation();
+                MapLocation robotLocation = rc.getLocation();
+                Direction moveDir = robotLocation.directionTo(enemyLocation);
+                if (rc.canMove(moveDir) && target == null) {
+                    rc.move(moveDir);
+                }
+            }
+        }
+
+        if (target != null) {
+
+            // If there is a target, attack
+            if (rc.canAttack(target.getLocation()))
+                rc.attack(target.getLocation());
+
+        }
 
 	}
 
