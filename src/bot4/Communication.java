7bdc25edde91b489d26e65fffa9bf324a61d8e5e
Mohit Hulse <hulsemohit@gmail.com>
2023-01-28 15:09:43 -0500

    tabs

--- src/bot4/Communication.java
+++ src/bot4/Communication.java
@@ -5,10 +5,10 @@ import battlecode.common.*;
 import bot4.util.*;
 
 public class Communication {
-    private static final int ARRAY_LENGTH = 64;
-    private static final int OUTDATED_TURNS = 2000;
+	private static final int ARRAY_LENGTH = 64;
+	private static final int OUTDATED_TURNS = 2000;
 
-    //static void writeMission(...)
-    //static void readMission(...)
-    //static void writeScout(...)
+	// static void writeMission(...)
+	// static void readMission(...)
+	// static void writeScout(...)
 }
--- src/bot4/util/MissionName.java
+++ src/bot4/util/MissionName.java
@@ -1,28 +1,26 @@
 package bot4.util;
 
 public enum MissionName {
+	START_UP(0),
 
-    START_UP(0),
+	PROTECT_HQ(1),
+	ATTACK_HQ(2),
+	PROTECT_ISLAND(3),
+	CAPTURE_ISLAND(4),
+	AMBUSH(5),
+	CREATE_ELIXIR_WELL(6),
+	UPGRADE_ADAMANTIUM_WELL(7),
+	UPGRADE_MANA_WELL(8),
+	COLLECT_ADAMANTIUM(9),
+	COLLECT_MANA(10),
+	COLLECT_ELIXIR(11),
 
-    PROTECT_HQ(1),
-    ATTACK_HQ(2),
-    PROTECT_ISLAND(3),
-    CAPTURE_ISLAND(4),
-    AMBUSH(5),
-    CREATE_ELIXIR_WELL(6),
-    UPGRADE_ADAMANTIUM_WELL(7),
-    UPGRADE_MANA_WELL(8),
-    COLLECT_ADAMANTIUM(9),
-    COLLECT_MANA(10),
-    COLLECT_ELIXIR(11),
+	SPEED_UP_HQ(12),
+	SCOUTING(13);
 
-    SPEED_UP_HQ(12),
-    SCOUTING(13);
+	public final int id;
 
-    public final int id;
-
-    private MissionName(int id) {
-        this.id = id;
-    }
-
+	private MissionName(int id) {
+		this.id = id;
+	}
 }
