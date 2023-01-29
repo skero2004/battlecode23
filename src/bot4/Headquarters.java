325f3990bc11fb384289a54ed70e59cda0c5f80b
Mohit Hulse <hulsemohit@gmail.com>
2023-01-29 13:05:51 -0500

    cleaned up hq and bugfixes (probably)

--- src/bot4/Headquarters.java
+++ src/bot4/Headquarters.java
@@ -5,29 +5,7 @@ import battlecode.common.*;
 import bot4.util.*;
 import bot4.Plan.Mission;
 
-import java.util.Random;
-
 public class Headquarters {
-	static final Random rng = new Random();
-	static final Direction[] directions = {
-			Direction.NORTH,
-			Direction.NORTHEAST,
-			Direction.EAST,
-			Direction.SOUTHEAST,
-			Direction.SOUTH,
-			Direction.SOUTHWEST,
-			Direction.WEST,
-			Direction.NORTHWEST,
-	};
-
-	static boolean init = true;
-	static Team myTeam = Team.NEUTRAL;
-	static void init(RobotController rc) throws GameActionException {
-		if (!init) return;
-		myTeam = rc.getTeam();
-
-		init = false;
-	}
 
 	/**
 	 * Responsibilities:
@@ -41,22 +19,63 @@ public class Headquarters {
 	 * - Pass it to Mapping
 	 */
 	static void run(RobotController rc) throws GameActionException {
-		init(rc);
+		Mission mission; // = Plan.chooseMission(rc);
+		switch (RobotPlayer.turnCount % 5) {
+			case 0:
+				mission = new Mission(MissionName.SCOUTING);
+				break;
+			case 1:
+				mission = new Mission(MissionName.COLLECT_MANA);
+				break;
+			case 2:
+				mission = new Mission(MissionName.COLLECT_ADAMANTIUM);
+				break;
+			case 3:
+				mission = new Mission(MissionName.CREATE_ANCHOR);
+				break;
+			case 4:
+				mission = new Mission(MissionName.CAPTURE_ISLAND);
+				break;
+			default:
+				throw new IllegalStateException("Math just broke");
+		}
 
-		// Choose mission
-		Mission mission = Plan.chooseMission(rc);
-		rc.setIndicatorString("T: " + mission.missionName);
+		rc.setIndicatorString("M: " + mission.missionName);
 
-		// Create anchor if mission tells it to
-		if (mission.missionName == MissionName.CREATE_ANCHOR) {
-			if (rc.canBuildAnchor(Anchor.STANDARD)) {
-				Plan.isCreateAnchor = false;
-				rc.buildAnchor(Anchor.STANDARD);
-			}
+		if (!target(rc, mission)) {
+			System.out.println("Skipping mission " + mission.missionName + " because no target specified");
 			return;
 		}
-			
-		// Execute mission
+
+		Communication.writeMission(rc, mission);
+
+		if (!build(rc, mission)) {
+			System.out.println("Skipping mission " + mission.missionName + " because could not build enough robots/anchors");
+			return;
+		}
+
+	}
+
+	private static boolean target(RobotController rc, Mission mission) throws GameActionException {
+		switch (mission.missionName) {
+			case COLLECT_ADAMANTIUM:
+			case COLLECT_MANA:
+				mission.target = Communication.readWell(rc, mission.getCollectResourceType());
+				return mission.target != null;
+			case CAPTURE_ISLAND:
+				mission.target = Communication.readIsland(rc, Team.NEUTRAL);
+				return mission.target != null;
+			case ATTACK_ISLAND:
+				mission.target = Communication.readIsland(rc, rc.getTeam().opponent());
+				return mission.target != null;
+			default:
+				mission.target = new MapLocation(Randomize.rng.nextInt(rc.getMapWidth()),
+						Randomize.rng.nextInt(rc.getMapHeight()));
+				return true;
+		}
+	}
+
+	private static boolean build(RobotController rc, Mission mission) throws GameActionException {
 		RobotType[] rt = {
 				RobotType.LAUNCHER,
 				RobotType.CARRIER,
@@ -64,6 +83,7 @@ public class Headquarters {
 				// RobotType.BOOSTER,
 				RobotType.AMPLIFIER,
 		};
+
 		int[] rn = {
 				mission.numLauncher,
 				mission.numCarrier,
@@ -71,24 +91,25 @@ public class Headquarters {
 				// mission.numBooster,
 				mission.numAmplifier,
 		};
-		// assert rt.length == rn.length;
-		// Communicate mission
-		if (mission.isValidCollectMission()) {
-			ResourceType type = mission.getCollectResourceType();
-			MapLocation loc = Communication.readWell(rc, type);
-			if (loc != null)
-				mission.target = loc;
+
+		switch (mission.missionName) {
+			case CREATE_ANCHOR:
+				if (rc.canBuildAnchor(Anchor.STANDARD)) {
+					Plan.isCreateAnchor = false;
+					rc.buildAnchor(Anchor.STANDARD);
+				} else {
+					return false;
+				}
+				break;
+			default:
+				break;
 		}
-		if (mission.target == null)
-			mission.target = new MapLocation(0, 0); // dummy
-		System.out.println("sending mission " + mission.missionName);
-		Communication.writeMission(rc, mission);
 
 		MapLocation cur = rc.getLocation();
 		for (int i = 0; i < rt.length; ++i) {
 			for (int j = 0; j < rn[i]; ++j) {
 				// Pick a direction to build in.
-				Direction dir = directions[rng.nextInt(directions.length)];
+				Direction dir = Constants.directions[Randomize.rng.nextInt(Constants.directions.length)];
 				MapLocation newLoc = cur.add(dir);
 				if (rc.canBuildRobot(rt[i], newLoc)) {
 					rc.buildRobot(rt[i], newLoc);
@@ -96,27 +117,7 @@ public class Headquarters {
 			}
 		}
 
-		// Communicate mission
-		if (mission.isValidCollectMission()) {
-			ResourceType type = mission.getCollectResourceType();
-			MapLocation loc = Communication.readWell(rc, type);
-			mission.target = loc;
-		} else if (mission.missionName == MissionName.CAPTURE_ISLAND ||
-				   mission.missionName == MissionName.ATTACK_ISLAND) {
-			MapLocation loc = Communication.readIsland(rc, Team.NEUTRAL);
-			if (loc == null) {
-				Team enemy = myTeam == Team.A ? Team.B : Team.A;
-				loc = Communication.readIsland(rc, enemy);
-			}
-			mission.target = loc;
-		} else mission.target = new MapLocation(0,0); // dummy
-
-		if (mission.target == null) {
-			System.out.println("skipping mission " + mission.missionName + " because no target specified");
-		} else {
-			System.out.println("sending mission " + mission.missionName);
-			Communication.writeMission(rc, mission);
-		}
+		return true;
 	}
 
 }
