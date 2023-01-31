ee1041e5f94b76b5a90a8b0f6ee9f687ec820cc4
Toya Takahashi <toyatakahashi522@gmail.com>
2023-01-31 16:52:55 -0500

    amplifiers follow

--- src/bot5a/Amplifier.java
+++ src/bot5a/Amplifier.java
@@ -6,63 +6,33 @@ import bot5a.util.*;
 
 public class Amplifier extends Robot {
 
-	private static boolean[][] vis = new boolean[GameConstants.MAP_MAX_WIDTH][GameConstants.MAP_MAX_HEIGHT];
-	private static WellInfo memoryWell;
-
-	private boolean init = false;
-	private Symmetry myMode;
-
 	void execute(RobotController rc) throws GameActionException {
 
-		if (!init) {
-
-			// 0 = check vertical, 1 = check rotational, 2 = check horz
-			if (rc.getID() % 3 == 0)
-				myMode = Symmetry.VERTICAL;
-			else if (rc.getID() % 3 == 1)
-				myMode = Symmetry.HORIZONTAL;
+		if (!followLeader(rc)) {
+			if (rc.getLocation().distanceSquaredTo(myMission.target) > 8)
+				move(rc);
 			else
-				myMode = Symmetry.ROTATIONAL;
-			init = true;
-
+				Scout.move(rc);
 		}
 
-		if (memoryWell != null)
-			rc.setIndicatorString(myMission.target + " " + memoryWell.getMapLocation() + " "
-					+ Map.symmetriesPossible.toString());
-
-		if (myMission.missionName == MissionName.FIND_SYMMETRY) {
-
-			if (myMission.target.equals(new MapLocation(61, 61))) {
-
-				move(rc, new MapLocation(Map.WIDTH / 2, Map.HEIGHT / 2));
-
-				WellInfo[] wells = rc.senseNearbyWells();
-
-				if (wells.length > 0) {
-					memoryWell = wells[0];
-					myMission.target = Map.reflect(wells[0].getMapLocation(), myMode);
-				}
-
-			} else {
-				move(rc);
-				if (rc.canSenseLocation(myMission.target)) {
-
-					WellInfo targetWell = rc.senseWell(myMission.target);
-					if (targetWell == null)
-						Map.symmetriesPossible.put(myMode, false);
-					else if (targetWell.getResourceType() != memoryWell.getResourceType())
-						Map.symmetriesPossible.put(myMode, false);
-				}
-			}
-
-			int m = rc.readSharedArray(63);
-			m |= (Map.symmetriesPossible.get(Symmetry.VERTICAL) ? 0 : 1) << 0;
-			m |= (Map.symmetriesPossible.get(Symmetry.HORIZONTAL) ? 0 : 1) << 1;
-			m |= (Map.symmetriesPossible.get(Symmetry.ROTATIONAL) ? 0 : 1) << 2;
-			rc.writeSharedArray(63, m);
-		} else
-			Scout.move(rc);
-
 	}
+
+	MapLocation[] last5;
+	int cooldown = 0;
+	private boolean followLeader(RobotController rc) throws GameActionException {
+		RobotInfo leader = null;
+		for (RobotInfo r : rc.senseNearbyRobots()) {
+			if (r.getTeam() == rc.getTeam() && r.getType() == RobotType.LAUNCHER
+					&& (leader == null || leader.getID() > r.getID()))
+				leader = r;
+		}
+		if (leader == null || leader.getID() > rc.getID())
+			return false;
+		if (rc.getLocation().distanceSquaredTo(leader.getLocation()) > 12)
+			stepTowards(rc, leader.getLocation());
+		else
+			Randomize.move(rc);
+		return true;
+	}
+
 }
